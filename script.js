bd23569171aa878b9cac78277f7ba31b63a8b465
hzumaeta <hzumaeta@gmail.com>
2023-06-01 21:07:49 -0500

    simple

--- script.js
+++ script.js
@@ -1,1 +1,90 @@
-console.log("Hola Mundo desde JavaScript");
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+let countyData
+let educationData
+
+let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
+
+let drawMap = () => {
+    canvas.selectAll('path')
+        .data(countyData)
+        .enter()
+        .append('path')
+        .attr('d', d3.geoPath())
+        .attr('class', 'county')
+        .attr('fill', (item) => {
+            let fips = item['id']
+            let county = educationData.find((county) => {
+                return county['fips'] === fips
+            })
+            let percentage = county['bachelorsOrHigher']
+            if (percentage <= 15) {
+                return '#FF0000'
+            } else if (percentage <= 30) {
+                return '#0000FF'
+            } else if (percentage <= 45) {
+                return '#00FF00'
+            } else {
+                return '#FFFF00'
+            }
+        })
+        .attr('data-fips', (item) => {
+            return item['id']
+        })
+        .attr('data-education', (item) => {
+            let fips = item['id']
+            let county = educationData.find((county) => {
+                return county['fips'] === fips
+            })
+            let percentage = county['bachelorsOrHigher']
+            return percentage
+        })
+        .on('mouseover', (countyDataItem) => {
+            tooltip.transition()
+                .style('visibility', 'visible')
+
+            let fips = countyDataItem['id']
+            let county = educationData.find((county) => {
+                return county['fips'] === fips
+            })
+
+            tooltip.text(county['fips'] + ' - ' + county['area_name'] + ', ' +
+                county['state'] + ' : ' + county['bachelorsOrHigher'] + '%')
+            tooltip.attr('data-education', county['bachelorsOrHigher'])
+        })
+        .on('mouseout', (countyDataItem) => {
+            tooltip.transition()
+                .style('visibility', 'hidden')
+        })
+
+}
+
+d3.json(countyURL).then(
+    (data, error) => {
+        if (error) {
+            console.log(error)
+        } else {
+            countyData = data
+            countyData = topojson.feature(countyData, countyData.objects.counties).features
+            console.log('County Data')
+            console.log(countyData)
+
+            d3.json(educationURL).then(
+                (data, error) => {
+                    if (error) {
+                        console.log(error)
+                    }
+                    else {
+                        educationData = data
+                        console.log('Education Data')
+                        console.log(educationData)
+                        drawMap()
+                    }
+                }
+            )
+
+        }
+    }
+)
